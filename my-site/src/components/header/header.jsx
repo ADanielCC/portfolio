6231c335b06938d95a892ebc50a755cc4cfa0be4
Daniel Adolfsson <daniel.adolfsson.93@gmail.com>
2023-06-27 16:17:59 +0200

    adding header files

---
+++ my-site/src/components/header/header.jsx
@@ -0,0 +1,41 @@
+import React, { Component, useCallback } from 'react';
+
+
+
+
+
+
+
+export default class Header extends Component{
+
+  constructor(props)
+  {
+      super(props);
+
+  }
+
+  state = {
+
+  }
+
+
+  
+  componentDidMount(){
+  
+  }
+
+  
+  render(){
+   
+
+    return(
+    <div>
+    
+
+    
+    </div>)
+  }
+
+}
+
+
