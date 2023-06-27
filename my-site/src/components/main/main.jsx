f4a2240eb3dbbc2f0014a0f9cbd4809ccdcfcdd7
Daniel Adolfsson <daniel.adolfsson.93@gmail.com>
2023-06-27 16:09:18 +0200

    adding files

---
+++ my-site/src/components/main/main.jsx
@@ -0,0 +1,41 @@
+import React, { Component, useCallback } from 'react';
+
+
+
+// import Header from '../header/header.jsx';
+
+
+
+export default class Main extends Component{
+
+  constructor(props)
+  {
+      super(props);
+
+  }
+
+  state = {
+
+  }
+
+
+  
+  componentDidMount(){
+  
+  }
+
+  
+  render(){
+   
+
+    return(
+    <div>
+        {/* <Header></Header> */}
+
+    
+    </div>)
+  }
+
+}
+
+
